bf71a744df385cab529e219487d4e96c232eb8dc
drohnet <drohne@gmail.com>
2017-01-20 16:47:36 +0100

    Add files via upload

---
+++ academy_raul_0120ca/__openerp__.py
@@ -0,0 +1,22 @@
+
+{
+    'name': 'Raul Academy v0120ca',
+    'version': '0.1',
+    'website' : 'https://gamesatpc.tk/',
+    'category': 'Tools',
+    'summary': 'Trainings',
+    'description': """
+Raul academy v2 correccion de persistencia de datos
+""",
+    'author': 'Raul',
+    'depends': ['base'],    
+    'data': [
+        'views/academy_views.xml',
+        'views/academy_menu.xml',
+        'academy_workfloud/course_workflow.xml',
+    ],
+    
+    'installable': True,
+    'auto_install': False,
+}
+
